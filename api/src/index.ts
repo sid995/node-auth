cac0539c38eb8ec509e9ba06cc4a2f0e50b28453
Pradeep <pradeepdesai063@gmail.com>
2020-02-29 12:52:29 +0530

    [Refactor:] refactored to make it modular

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,14 @@
-import express from "express";
 import session from "express-session";
 import Redis from "ioredis";
 import connectRedis from "connect-redis";
 import mongoose from "mongoose";
 import {
 	REDIS_OPTIONS,
-	SESSION_OPTIONS,
 	APP_PORT,
 	MONGO_URI,
 	MONGO_OPTIONS
-} from "./config";
+} from "./config/index";
+import { createApp } from "./app";
 
 (async () => {
 	await mongoose.connect(MONGO_URI, MONGO_OPTIONS);
@@ -18,11 +17,9 @@ import {
 
 	const client = new Redis(REDIS_OPTIONS);
 
-	const app = express();
+	const store = new RedisStore({ client });
 
-	app.use(session({ ...SESSION_OPTIONS, store: new RedisStore({ client }) }));
-
-	app.get("/", (req, res) => res.json({ message: "OK" }));
+	const app = createApp(store);
 
 	app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}`));
 })();
